f8ee0b401976269e199a0b4caa133b5bc5403221
Daniel Zhang <zhang00710@sina.com>
2019-09-28 16:33:19 +0800

    add draggable motions for the dog images

--- dog.js
+++ dog.js
@@ -20,8 +20,34 @@ function addDoggos() {
         img.src = processedResponse.message;
         img.alt = "Cute Dog";
         img.classList.add("dog-img");
+        addMotion(img);
         doggos.appendChild(img);
         spinner.setAttribute("style","");
     });
     spinner.setAttribute("style","display: block");
+}
+
+const { styler, spring, listen, pointer, value } = window.popmotion;
+
+function addMotion(img) {
+    const divStyler = styler(img);
+    const imgXY = value({ x: 0, y: 0 }, divStyler.set);
+
+    listen(img, 'mousedown touchstart')
+    .start((e) => {
+        e.preventDefault();
+        pointer(imgXY.get()).start(imgXY);
+    });
+
+    listen(document, 'mouseup touchend')
+    .start(() => {
+        spring({
+        from: imgXY.get(),
+        velocity: imgXY.getVelocity(),
+        to: { x: 0, y: 0 },
+        stiffness: 200,
+        // mass: 1,
+        // damping: 10
+        }).start(imgXY);
+    });
 }
