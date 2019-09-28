339de4819aeffdf13ee87fdfce0101c5adb3d5aa
Daniel Zhang <zhang00710@sina.com>
2019-09-28 15:46:05 +0800

    initial commit for the html,css,js

---
+++ dog.js
@@ -0,0 +1,26 @@
+const DOG_URL = "https://dog.ceo/api/breeds/image/random/";
+
+const doggos = document.querySelector('.doggos')
+
+const button = document.querySelector('.main-btn');
+
+const spinner = document.querySelector('.spinner');
+
+button.addEventListener("click",addDoggos);
+
+function addDoggos() {
+    const promise = fetch(DOG_URL);
+    promise
+    .then(function (response) {
+        const processingPromise = response.json();
+        return processingPromise;
+    })
+    .then(function (processedResponse) {
+        const img = document.createElement("img");
+        img.src = processedResponse.message;
+        img.alt = "Cute Dog";
+        doggos.appendChild(img);
+        spinner.setAttribute("style","display: none");
+    });
+    spinner.setAttribute("style","display: block");
+}
