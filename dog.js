e1556588fc84d7c49e94c2193872de53846d0101
Daniel Zhang <zhang00710@sina.com>
2019-09-28 22:37:10 +0800

    1. add breed select; 2. fixed a missing ";" at dog.js.

--- dog.js
+++ dog.js
@@ -1,15 +1,83 @@
 const DOG_URL = "https://dog.ceo/api/breeds/image/random/";
 
-const doggos = document.querySelector('.doggos')
+const BREEDS_LIST_URL = "https://dog.ceo/api/breeds/list/all";
+
+const DOG_URL_BY_BREED_PREFIX = "https://dog.ceo/api/breed/";
+
+const DOG_URL_BY_BREED_SUFFIX = "/images/random"
+
+let breedsList = {};
+
+const doggos = document.querySelector('.doggos');
 
 const button = document.querySelector('.main-btn');
 
 const spinner = document.querySelector('.spinner');
 
+const selector = document.querySelector('.breed-select');
+
 button.addEventListener("click",addDoggos);
 
+getBreedsList();
+
+function getBreedsList() {
+    const promise = fetch(BREEDS_LIST_URL);
+    promise
+        .then(function(response) {
+            processingPromise = response.json();
+            return processingPromise;
+        })
+        .then(function(processedPromise) {
+            breedsList = processedPromise.message;
+            updateSelect(breedsList);
+        })
+}
+
+function updateSelect(breedsList){
+    const listCache = document.createElement("select");
+    listCache.name = selector.name;
+    selector.classList.forEach(function (c) {
+        listCache.classList.add(c);
+    });
+    const optionAll = document.createElement("option");
+    const listParent = selector.parentNode;
+    optionAll.value = "";
+    optionAll.innerText = "all breed";
+    listCache.appendChild(optionAll);
+    for (breed in breedsList) {
+        if (breedsList[breed].length === 0) {
+            const breedOption = document.createElement("option");
+            breedOption.value = breed;
+            breedOption.innerText = breed;
+            listCache.appendChild(breedOption);
+        } else if (breedsList[breed].length > 0) {
+            const breedOptgroup = document.createElement("optgroup");
+            breedOptgroup.label = breed;
+            const subOptionAll = document.createElement("option");
+            subOptionAll.value = breed;
+            subOptionAll.innerText = "all " + breed;
+            breedOptgroup.appendChild(subOptionAll);
+            breedsList[breed].forEach(function (subbreed) {
+                const subOption = document.createElement("option");
+                subOption.value = breed + "/" +subbreed;
+                subOption.innerText = subbreed;
+                breedOptgroup.appendChild(subOption);
+            })
+            listCache.appendChild(breedOptgroup);
+        }
+    }
+    selector.remove();
+    listParent.appendChild(listCache);
+
+}
+
 function addDoggos() {
-    const promise = fetch(DOG_URL);
+    const selectedBreed = document.querySelector('.breed-select').selectedOptions[0].value;
+    let url = DOG_URL;
+    if (selectedBreed !== "") {
+        url = DOG_URL_BY_BREED_PREFIX + selectedBreed + DOG_URL_BY_BREED_SUFFIX;
+    }
+    const promise = fetch(url);
     promise
     .then(function (response) {
         const processingPromise = response.json();
@@ -18,7 +86,7 @@ function addDoggos() {
     .then(function (processedResponse) {
         const img = document.createElement("img");
         img.src = processedResponse.message;
-        img.alt = "Cute Dog";
+        img.alt = "Cute Dog: " + selectedBreed.replace("/"," ");
         img.classList.add("dog-img");
         addMotion(img);
         doggos.appendChild(img);
